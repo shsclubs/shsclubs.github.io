a2a1bb45499e061361f1e8997167876c5281b5f3
cqb13 <cqb13.main@gmail.com>
2024-03-05 23:11:06 -0500

    fixed duplicate entries & small css changes

--- populatePage.js
+++ populatePage.js
@@ -11,7 +11,7 @@ function updatePageNameAndDescription(category) {
   pageDescription.innerHTML = "Clubs to learn about " + prettyName + ".";
 }
 
-function addClub(club) {
+function addClub(club, odd) {
   let button =
     '<a href="#' + club.nick + '"><button>' + club.nick + "</button></a>";
   let name = '<a id="' + club.nick + '"></a><h3>' + club.name + "</h3>";
@@ -21,22 +21,32 @@ function addClub(club) {
   let meetingdays =
     "<p><strong>Meeting Days:</strong> " + club.meetingdays + "</p>";
   let loc = "<p><strong>Location:</strong> " + club.loc + "</p>";
-  let text = name + desc + adv + stucon + meetingdays + loc;
-  clubDiv.innerHTML += text;
+  let text =
+    `<div ${odd ? 'class="odd-club club"' : "club"}>` +
+    name +
+    desc +
+    adv +
+    stucon +
+    meetingdays +
+    loc +
+    "</div>";
   buttonDiv.innerHTML += button;
+  clubDiv.innerHTML += text;
 }
 
 function buildPage() {
   const category = getCategoryFromURL();
   if (!category) return;
   updatePageNameAndDescription(category);
-
+  let clubsOnPage = 0;
   for (let i = 0; i < clubList.length; i++) {
     let club = clubList[i];
-    if (club.type1 === category) {
-      addClub(club);
+    if (club.type1 === category || club.type2 === category) {
+      addClub(club, (clubsOnPage + 1) % 2 === 0);
+      clubsOnPage += 1;
     } else if (club.type2 === category) {
-      addClub(club);
+      addClub(club, (clubsOnPage + 1) % 2 === 0);
+      clubsOnPage += 1;
     }
   }
 }
@@ -51,5 +61,3 @@ function getCategoryFromURL() {
 document.addEventListener("DOMContentLoaded", function () {
   buildPage();
 });
-
-buildPage();
